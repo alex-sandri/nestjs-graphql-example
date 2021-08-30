a4303787c2991ff4e0e94820f7b45d310754d50a
Alex Sandri <me@alexsandri.com>
2021-08-30 10:25:32 +0200

    first commit

---
+++ src/app.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { UsersModule } from './users/users.module';
+
+@Module({
+  imports: [
+    GraphQLModule.forRoot({
+      autoSchemaFile: true,
+    }),
+    UsersModule,
+  ],
+})
+export class AppModule {}
