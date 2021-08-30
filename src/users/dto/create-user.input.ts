645b02e0a976bf218373f0eee21f4ec63e9704f4
Alex Sandri <me@alexsandri.com>
2021-08-30 10:42:15 +0200

    Update user schema

--- src/users/dto/create-user.input.ts
+++ src/users/dto/create-user.input.ts
@@ -1,7 +1,19 @@
-import { InputType, Int, Field } from '@nestjs/graphql';
+import { InputType, Field } from '@nestjs/graphql';
+import { IsEmail, IsNotEmpty, MinLength } from 'class-validator';
 
 @InputType()
 export class CreateUserInput {
-  @Field(() => Int, { description: 'Example field (placeholder)' })
-  exampleField: number;
+  @Field(() => String)
+  @IsNotEmpty()
+  name: string;
+
+  @Field(() => String)
+  @IsEmail()
+  @IsNotEmpty()
+  email: string;
+
+  @Field(() => String)
+  @IsNotEmpty()
+  @MinLength(8)
+  password: string;
 }
--- src/users/dto/update-user.input.ts
+++ src/users/dto/update-user.input.ts
@@ -1,8 +1,24 @@
 import { CreateUserInput } from './create-user.input';
-import { InputType, Field, Int, PartialType } from '@nestjs/graphql';
+import { InputType, Field, PartialType, Int } from '@nestjs/graphql';
+import { IsEmail, IsNotEmpty, IsOptional, MinLength } from 'class-validator';
 
 @InputType()
 export class UpdateUserInput extends PartialType(CreateUserInput) {
   @Field(() => Int)
+  @IsNotEmpty()
   id: number;
+
+  @Field(() => String)
+  @IsOptional()
+  name?: string;
+
+  @Field(() => String)
+  @IsEmail()
+  @IsOptional()
+  email?: string;
+
+  @Field(() => String)
+  @IsOptional()
+  @MinLength(8)
+  password?: string;
 }
