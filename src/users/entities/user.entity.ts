29b4431556a0b04a44cde4a9a8b0cf95e740c610
Alex Sandri <me@alexsandri.com>
2021-08-30 11:15:34 +0200

    Update user entity

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,7 +1,18 @@
-import { ObjectType, Field, Int } from '@nestjs/graphql';
+import { ObjectType, Field, Int, HideField } from '@nestjs/graphql';
 
 @ObjectType()
 export class User {
   @Field(() => Int)
+  @HideField()
   id: number;
+
+  @Field(() => String)
+  name: string;
+
+  @Field(() => String)
+  email: string;
+
+  @Field(() => String)
+  @HideField()
+  password: string;
 }
